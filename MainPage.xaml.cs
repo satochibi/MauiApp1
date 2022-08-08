8d41959b10aeb917240c6e60990178fc54dfc64e
satochibi <satochibi0120@gmail.com>
2022-08-08 10:59:42 +0900

    spreadsheetgearのサンプルを作成

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -14,8 +14,22 @@ public partial class MainPage : ContentPage
 
     async void OnButtonClicked(object sender, EventArgs args)
     {
-        Button button = (Button)sender;
-        await DisplayAlert("Clicked!", "The button labeled '" + button.Text + "' has been clicked", "OK");
+        // Button button = (Button)sender;
+        // await DisplayAlert("Clicked!", "The button labeled '" + button.Text + "' has been clicked", "OK");
+        
+        // Create a new empty workbook in a new workbook set.
+        SpreadsheetGear.IWorkbook workbook = SpreadsheetGear.Factory.GetWorkbook();
+
+        // Get a reference to the first worksheet.
+        SpreadsheetGear.IWorksheet worksheet = workbook.Worksheets["Sheet1"];
+
+        // Get a reference to the top left cell of Sheet1.
+        SpreadsheetGear.IRange a1 = worksheet.Cells["A1"];
+
+        // Set a formula.
+        a1.Formula = "=24901.55 / PI()";
+
+        await DisplayAlert("clicked",a1.Value.ToString(), "ok");
     }
 
 }
